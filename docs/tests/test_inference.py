0931253ac2c5a053b7ac7499257cd93b36a7b86c
kostastsa <kostas.tsampourakis@gmail.com>
2023-04-06 17:38:55 +0100

    added changes

--- docs/tests/test_inference.py
+++ docs/tests/test_inference.py
@@ -1,11 +1,11 @@
 from jax import numpy as jnp
-from jax import jacfwd, jacrev, jit, vmap
+from jax import jacfwd, jacrev, jit, vmap, lax
 from jax import random as jr
 from jax import tree_util as jtu
 
 import gaussfiltax.utils as utils
 import gaussfiltax.containers as containers
-from dynamax.nonlinear_gaussian_ssm.models import ParamsNLGSSM, NonlinearGaussianSSM
+from gaussfiltax.models import ParamsNLSSM, NonlinearGaussianSSM, GeneralNonlinearSSM
 from tensorflow_probability.substrates.jax.distributions import MultivariateNormalFullCovariance as MVN
 import time
 import gaussfiltax.inference as gf
@@ -41,13 +41,15 @@ class TestInference:
 
     # Model definition 
     model = NonlinearGaussianSSM(state_dim, emission_dim)
-    params = ParamsNLGSSM(
+    params = ParamsNLSSM(
         initial_mean=mu0,
         initial_covariance=Sigma0,
         dynamics_function=f3,
-        dynamics_covariance=Q,
+        dynamics_noise_bias=jnp.zeros(Q.shape[0]),
+        dynamics_noise_covariance=Q,
         emission_function=g3,
-        emission_covariance=R,
+        emission_noise_bias=jnp.zeros(R.shape[0]),
+        emission_noise_covariance=R,
     )
 
     # Generate synthetic data 
@@ -63,18 +65,68 @@ class TestInference:
         num_components = [5, 3, 3] # has to be set correctly OW "TypeError: Cannot interpret '<function <lambda> at 0x12eae3ee0>' as a data type". Check internal containers._branch_from_node
         posterior_filtered, aux_outputs = gf.augmented_gaussian_sum_filter(self.params, self.emissions, num_components, opt_args = (20, 0.001, 50), inputs=self.inputs)
 
-test = TestInference()
+# test = TestInference()
 
-tin = time.time()
-test.test_gaussian_sum_filter()
-tout = time.time()
-print('GSF time:', tout - tin)
+# tin = time.time()
+# test.test_gaussian_sum_filter()
+# tout = time.time()
+# print('GSF time:', tout - tin)
 
-tin = time.time()
-test.test_augmented_gaussian_sum_filter()
-tout = time.time()
-print('AGSF time:', tout - tin)
-
-
+# tin = time.time()
+# test.test_augmented_gaussian_sum_filter()
+# tout = time.time()
+# print('AGSF time:', tout - tin)
 
 
+
+
+# Parameters
+state_dim = 1
+state_noise_dim = 1
+emission_dim = 1
+emission_noise_dim = 1
+seq_length = 500
+mu0 = jnp.zeros(state_dim)
+Sigma0 = 1.0 * jnp.eye(state_dim)
+Q = 1.0 * jnp.eye(state_noise_dim)
+R = 1.0 * jnp.eye(emission_noise_dim)
+
+# Nonlinearities
+alpha = 0.91
+sigma = 1.0
+beta = 0.5
+f = lambda x, q, u: alpha * x + sigma * q
+h = lambda x, r, u: beta * jnp.exp(x/2) * r
+
+# Inputs
+inputs = 8. * jnp.cos(jnp.arange(seq_length))
+
+# Model definition 
+model = GeneralNonlinearSSM(state_dim, state_noise_dim, emission_dim, emission_noise_dim)
+params = ParamsNLSSM(
+    initial_mean=mu0,
+    initial_covariance=Sigma0,
+    dynamics_function=f,
+    dynamics_noise_bias=jnp.zeros(state_noise_dim),
+    dynamics_noise_covariance=Q,
+    emission_function=h,
+    emission_noise_bias=jnp.zeros(emission_noise_dim),
+    emission_noise_covariance=R,
+)
+
+# Generate synthetic data 
+key = jr.PRNGKey(0)
+states, emissions = model.sample(params, key, seq_length, inputs = inputs)
+
+
+num_components = 5
+posterior_filtered_gsf = gf.gaussian_sum_filter(params, emissions, num_components, 1, inputs)
+point_estimate_gsf = jnp.sum(jnp.einsum('ijk,ij->ijk', posterior_filtered_gsf.means, posterior_filtered_gsf.weights), axis=0)
+
+
+# Plot synthetic data
+plt.plot(states)
+plt.plot(point_estimate_gsf)
+plt.plot(emissions)
+plt.legend(['x','gsf', 'y'])
+plt.show()
---
+++ gaussfiltax/models.py
@@ -0,0 +1,255 @@
+from jaxtyping import Array, Float
+from typing import NamedTuple, Optional, Union, Callable, Tuple
+import tensorflow_probability.substrates.jax as tfp
+from tensorflow_probability.substrates.jax.distributions import MultivariateNormalFullCovariance as MVN
+import tensorflow_probability.substrates.jax.distributions as tfd
+from dynamax.ssm import SSM
+from dynamax.parameters import ParameterSet, PropertySet
+from dynamax.types import PRNGKey, Scalar
+from jax import jit, lax, vmap
+from jax.tree_util import tree_map
+import jax.numpy as jnp
+import jax.random as jr
+
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+
+
+FnStateToState = Callable[ [Float[Array, "state_dim"]], Float[Array, "state_dim"]]
+FnStateAndInputToState = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"]], Float[Array, "state_dim"]]
+FnStateToEmission = Callable[ [Float[Array, "state_dim"]], Float[Array, "emission_dim"]]
+FnStateAndInputToEmission = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"] ], Float[Array, "emission_dim"]]
+
+
+class ParamsNLSSM(NamedTuple):
+    """Parameters for a NLGSSM model.
+
+    $$p(z_t | z_{t-1}, u_t) = N(z_t | f(z_{t-1}, u_t), Q_t)$$
+    $$p(y_t | z_t) = N(y_t | h(z_t, u_t), R_t)$$
+    $$p(z_1) = N(z_1 | m, S)$$
+
+    If you have no inputs, the dynamics and emission functions do not to take $u_t$ as an argument.
+
+    :param dynamics_function: $f$
+    :param dynamics_covariance: $Q$
+    :param emissions_function: $h$
+    :param emissions_covariance: $R$
+    :param initial_mean: $m$
+    :param initial_covariance: $S$
+
+    """
+
+    initial_mean: Float[Array, "state_dim"]
+    initial_covariance: Float[Array, "state_dim state_dim"]
+    dynamics_function: Union[FnStateToState, FnStateAndInputToState]
+    dynamics_noise_bias: Float[Array, "state_noise_dim"]
+    dynamics_noise_covariance: Float[Array, "state_noise_dim state_noise_dim"]
+    emission_function: Union[FnStateToEmission, FnStateAndInputToEmission]
+    emission_noise_bias: Float[Array, "emission_noise_dim"] 
+    emission_noise_covariance: Float[Array, "emission_noise_dim emission_noise_dim"]
+
+class NonlinearGaussianSSM(SSM):
+    """
+    Nonlinear Gaussian State Space Model.
+
+    The model is defined as follows
+
+    $$p(z_t | z_{t-1}, u_t) = N(z_t | f(z_{t-1}, u_t), Q_t)$$
+    $$p(y_t | z_t) = N(y_t | h(z_t, u_t), R_t)$$
+    $$p(z_1) = N(z_1 | m, S)$$
+
+    where the model parameters are
+
+    * $z_t$ = hidden variables of size `state_dim`,
+    * $y_t$ = observed variables of size `emission_dim`
+    * $u_t$ = input covariates of size `input_dim` (defaults to 0).
+    * $f$ = dynamics (transition) function
+    * $h$ = emission (observation) function
+    * $Q$ = covariance matrix of dynamics (system) noise
+    * $R$ = covariance matrix for emission (observation) noise
+    * $m$ = mean of initial state
+    * $S$ = covariance matrix of initial state
+
+
+    These parameters of the model are stored in a separate object of type :class:`ParamsNLGSSM`.
+    """
+
+
+    def __init__(self, state_dim: int, emission_dim: int, input_dim: int = 0):
+        self.state_dim = state_dim
+        self.emission_dim = emission_dim
+        self.input_dim = 0
+
+    @property
+    def emission_shape(self):
+        return (self.emission_dim,)
+
+    @property
+    def inputs_shape(self):
+        return (self.input_dim,) if self.input_dim > 0 else None
+
+    def initial_distribution(
+        self,
+        params: ParamsNLSSM,
+        inputs: Optional[Float[Array, "input_dim"]] = None
+    ) -> tfd.Distribution:
+        return MVN(params.initial_mean, params.initial_covariance)
+
+    def transition_distribution(
+        self,
+        params: ParamsNLSSM,
+        state: Float[Array, "state_dim"],
+        inputs: Optional[Float[Array, "input_dim"]] = None
+    ) -> tfd.Distribution:
+        f = params.dynamics_function
+        if inputs is None:
+            mean = f(state)
+        else:
+            mean = f(state, inputs)
+        return MVN(mean, params.dynamics_noise_covariance)
+
+    def emission_distribution(
+        self,
+        params: ParamsNLSSM,
+        state: Float[Array, "state_dim"],
+        inputs: Optional[Float[Array, "input_dim"]] = None
+     ) -> tfd.Distribution:
+        h = params.emission_function
+        if inputs is None:
+            mean = h(state)
+        else:
+            mean = h(state, inputs)
+        return MVN(mean, params.emission_noise_covariance)
+    
+
+class GeneralNonlinearSSM(SSM):
+    """
+    General Nonlinear State Space Model.
+
+    The model is defined as follows
+
+    $$z_t = f(z_{t-1}, q_t, u_t)$$
+    $$y_t = h(z_t, r_t, u_t)$$
+
+    where 
+
+    $$q_t ~ N(0, Q_t)$$
+    $$r_t ~ N(0, R_t)$$
+    $$z_1 ~ N(m, S)$$
+
+    and the model parameters are
+
+    * $z_t$ = hidden variables of size `state_dim`,
+    * $y_t$ = observed variables of size `emission_dim`
+    * $u_t$ = input covariates of size `input_dim` (defaults to 0).
+    * $f$ = dynamics (transition) function
+    * $h$ = emission (observation) function
+    * $Q$ = covariance matrix of dynamics (system) noise
+    * $R$ = covariance matrix for emission (observation) noise
+    * $m$ = mean of initial state
+    * $S$ = covariance matrix of initial state
+
+
+    These parameters of the model are stored in a separate object of type :class:`ParamsNLGSSM`.
+    """
+
+
+    def __init__(self, state_dim: int, state_noise_dim:int,  emission_dim: int, emission_noise_dim:int, input_dim: int = 0):
+        self.state_dim = state_dim
+        self.state_noise_dim = state_noise_dim
+        self.emission_dim = emission_dim
+        self.emission_noise_dim = emission_noise_dim
+        self.input_dim = 0
+
+    @property
+    def emission_shape(self):
+        return (self.emission_dim,)
+
+    @property
+    def inputs_shape(self):
+        return (self.input_dim,) if self.input_dim > 0 else None
+
+    def initial_distribution(
+        self,
+        params: ParamsNLSSM,
+        inputs: Optional[Float[Array, "input_dim"]] = None
+    ) -> tfd.Distribution:
+        return MVN(params.initial_mean, params.initial_covariance)
+
+    def transition_distribution(
+        self,
+        params: ParamsNLSSM,
+        state: Float[Array, "state_dim"],
+        inputs: Optional[Float[Array, "input_dim"]] = None
+    ) -> tfd.Distribution:
+        f = params.dynamics_function
+        if inputs is None:
+            mean = f(state)
+        else:
+            mean = f(state, inputs)
+        return MVN(mean, params.dynamics_noise_covariance)
+
+    def emission_distribution(
+        self,
+        params: ParamsNLSSM,
+        state: Float[Array, "state_dim"],
+        inputs: Optional[Float[Array, "input_dim"]] = None
+     ) -> tfd.Distribution:
+        h = params.emission_function
+        if inputs is None:
+            mean = h(state)
+        else:
+            mean = h(state, inputs)
+        return MVN(mean, params.emission_noise_covariance)
+    
+    def sample(
+        self,
+        params: ParameterSet,
+        key: PRNGKey,
+        num_timesteps: int,
+        inputs: Optional[Float[Array, "num_timesteps input_dim"]]=None
+    ) -> Tuple[Float[Array, "num_timesteps state_dim"],
+              Float[Array, "num_timesteps emission_dim"]]:
+        r"""Sample states $z_{1:T}$ and emissions $y_{1:T}$ given parameters $\theta$ and (optionally) inputs $u_{1:T}$.
+
+        Args:
+            params: model parameters $\theta$
+            key: random number generator
+            num_timesteps: number of timesteps $T$
+            inputs: inputs $u_{1:T}$
+
+        Returns:
+            latent states and emissions
+
+        """
+
+        f = params.dynamics_function
+        h = params.emission_function
+
+        def _step(prev_state, args):
+            key, inpt = args
+            key1, key2 = jr.split(key, 2)
+            q = MVN(params.dynamics_noise_bias, params.dynamics_noise_covariance).sample(seed=key1)
+            r = MVN(params.emission_noise_bias, params.emission_noise_covariance).sample(seed=key2)
+            state = f(prev_state, q, inpt)
+            emission = h(state, r, inpt)
+            return state, (state, emission)
+
+        # Sample the initial state
+        key1, key2, key = jr.split(key, 3)
+        initial_input = tree_map(lambda x: x[0], inputs)
+        initial_state = self.initial_distribution(params, initial_input).sample(seed=key1)
+        r0 = MVN(params.emission_noise_bias, params.emission_noise_covariance).sample(seed=key2)
+        initial_emission = h(initial_state, r0, initial_input)
+
+        # Sample the remaining emissions and states
+        next_keys = jr.split(key, num_timesteps - 1)
+        next_inputs = tree_map(lambda x: x[1:], inputs)
+        _, (next_states, next_emissions) = lax.scan(_step, initial_state, (next_keys, next_inputs))
+
+        # Concatenate the initial state and emission with the following ones
+        expand_and_cat = lambda x0, x1T: jnp.concatenate((jnp.expand_dims(x0, 0), x1T))
+        states = tree_map(expand_and_cat, initial_state, next_states)
+        emissions = tree_map(expand_and_cat, initial_emission, next_emissions)
+        return states, emissions
